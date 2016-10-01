8c85e71c2875cc1c6248e26f122ff526e772534d
michprev <michprev@gmail.com>
2016-10-01 12:20:01 +0200

    Fix reading raw temperature and pressure values; add low temperature compensation

--- Barometer/MS5611.cpp
+++ Barometer/MS5611.cpp
@@ -41,34 +41,51 @@ HAL_StatusTypeDef MS5611::GetData(int32_t * temperature, int32_t * pressure)
 {
 	HAL_StatusTypeDef status;
 	uint32_t D1, D2;
-	uint8_t conversionData[4];
+	uint8_t conversionData[3];
 
 	uint8_t convertD1 = 0x48;
 	status = HAL_I2C_Master_Transmit(this->hi2c, this->ADDRESS, &convertD1, 1, HAL_MAX_DELAY);
 
 	HAL_Delay(10);
 
-	status = HAL_I2C_Mem_Read(this->hi2c, this->ADDRESS, 0x00, I2C_MEMADD_SIZE_8BIT, conversionData, 4, HAL_MAX_DELAY);
-	D1 = conversionData[0] << 24;
-	D1 |= conversionData[1] << 16;
-	D1 |= conversionData[2] << 8;
-	D1 |= conversionData[3];
+	status = HAL_I2C_Mem_Read(this->hi2c, this->ADDRESS, 0x00, I2C_MEMADD_SIZE_8BIT, conversionData, 3, HAL_MAX_DELAY);
+	D1 = conversionData[0] << 16;
+	D1 |= conversionData[1] << 8;
+	D1 |= conversionData[2];
 
 	uint8_t convertD2 = 0x58;
 	status = HAL_I2C_Master_Transmit(this->hi2c, this->ADDRESS, &convertD2, 1, HAL_MAX_DELAY);
 
 	HAL_Delay(10);
 
-	status = HAL_I2C_Mem_Read(this->hi2c, this->ADDRESS, 0x00, I2C_MEMADD_SIZE_8BIT, conversionData, 4, HAL_MAX_DELAY);
-	D2 = conversionData[0] << 24;
-	D2 |= conversionData[1] << 16;
-	D2 |= conversionData[2] << 8;
-	D2 |= conversionData[3];
+	status = HAL_I2C_Mem_Read(this->hi2c, this->ADDRESS, 0x00, I2C_MEMADD_SIZE_8BIT, conversionData, 3, HAL_MAX_DELAY);
+	D2 = conversionData[0] << 16;
+	D2 |= conversionData[1] << 8;
+	D2 |= conversionData[2];
 
-	int32_t dT = D2 - C[4] * pow(2, 8);
+	int32_t dT = D2 - (uint32_t)C[4] * pow(2, 8);
 	int32_t TEMP = 2000 + dT * C[5] / pow(2, 23);
-	int64_t OFF = C[1] * pow(2, 16) + ((double)(C[3] * dT)) / pow(2, 7);
-	int64_t SENS = C[0] * pow(2, 15) + ((double)(C[2] * dT)) / pow(2, 8);
+	int64_t OFF = (int64_t)C[1] * pow(2, 16) + (int64_t)(C[3] * dT) / pow(2, 7);
+	int64_t SENS = (int64_t)C[0] * pow(2, 15) + (int64_t)(C[2] * dT) / pow(2, 8);
+
+	int32_t TEMP2 = 0;
+	int64_t OFF2 = 0;
+	int64_t SENS2 = 0;
+
+	if (TEMP < 2000) {
+		TEMP2 = (dT * dT) / pow(2, 31);
+		OFF2 = 5 * ((TEMP - 2000) * (TEMP - 2000)) / 2;
+		SENS2 = 5 * ((TEMP - 2000) * (TEMP - 2000)) / 4;
+	}
+	if (TEMP < -1500) {
+		OFF2 = OFF2 + 7 * ((TEMP + 1500) * (TEMP + 1500));
+		SENS2 = SENS2 + 11 * ((TEMP + 1500) * (TEMP + 1500)) / 2;
+	}
+
+	TEMP -= TEMP2;
+	OFF -= OFF2;
+	SENS -= SENS2;
+
 	int32_t P = (((double)D1) * SENS / pow(2, 21) - OFF) / pow(2, 15);
 
 	*temperature = TEMP;
